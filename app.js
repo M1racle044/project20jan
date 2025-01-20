bc5305c8664e2981926db4833008768980e64841
Alibek <alibekmiracle@gmail.com>
2025-01-20 20:37:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// let = ['apple','banana','orange','mango']
+// let i = 0
+// while(i < Array.length){
+//     i++
+// }
+//     console.log(arr[i])
+for(let i = 0; 1 > -1; i++){
+    console.log(true)
+}
